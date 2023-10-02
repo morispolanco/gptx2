c297e96c2c48f6ca1b602d76e313b20e00b369b5
morispolanco <morispolanco@gmail.com>
2023-10-02 03:28:16 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,38 +11,51 @@ else:
     # Continuar con el resto del código que utiliza la clave de API
 
 # Título de la aplicación
-st.title("ChatGPT")
+st.title("Chatbots de GPT-3")
 
-# Campo de entrada de texto
-user_input = st.text_input("Usuario:", "")
+# Ingresar el mensaje inicial
+mensaje_inicial = st.text_input("Ingresa tu mensaje inicial")
 
-# Conversación inicial
-conversation = []
+# Conversación entre los chatbots
+def conversacion(mensaje_inicial):
+    # Inicializar la conversación con el mensaje inicial
+    conversacion = [mensaje_inicial]
 
-# Función para generar una respuesta de ChatGPT
-def generar_respuesta(input_text, conversation):
-    # Agregar la entrada del usuario a la conversación
-    conversation.append({"role": "user", "content": input_text})
-    
-    # Generar una respuesta de ChatGPT
-    respuesta = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=conversation,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-    
-    # Agregar la respuesta de ChatGPT a la conversación
-    conversation.append({"role": "assistant", "content": respuesta.choices[0].text.strip()})
-    
-    return respuesta.choices[0].text.strip()
+    # Bucle para el diálogo entre los chatbots
+    while True:
+        # Obtener la respuesta del primer chatbot
+        respuesta_1 = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=conversacion,
+            max_tokens=50,
+            temperature=0.7
+        )
 
-# Botón para enviar la entrada del usuario y generar una respuesta
-if st.button("Enviar"):
-    if user_input:
-        respuesta = generar_respuesta(user_input, conversation)
-        st.text_area("ChatGPT:", value=respuesta, height=200, max_chars=None, key=None)
-    else:
-        st.warning("Por favor, ingresa un mensaje.")
+        # Agregar la respuesta del primer chatbot a la conversación
+        conversacion.append(respuesta_1.choices[0].text.strip())
+
+        # Obtener la respuesta del segundo chatbot
+        respuesta_2 = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=conversacion,
+            max_tokens=50,
+            temperature=0.7
+        )
+
+        # Agregar la respuesta del segundo chatbot a la conversación
+        conversacion.append(respuesta_2.choices[0].text.strip())
+
+        # Mostrar la conversación en la interfaz de la aplicación
+        st.write("Chatbot 1:", conversacion[-2])
+        st.write("Chatbot 2:", conversacion[-1])
+
+        # Preguntar al usuario si desea continuar la conversación
+        continuar = st.selectbox("Continuar la conversación?", ["Sí", "No"])
+
+        # Terminar la conversación si el usuario elige "No"
+        if continuar == "No":
+            break
+
+# Iniciar la conversación si se ha ingresado un mensaje inicial
+if mensaje_inicial:
+    conversacion(mensaje_inicial)
